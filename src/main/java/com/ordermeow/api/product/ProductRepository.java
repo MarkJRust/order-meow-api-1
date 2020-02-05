924a3c9dc2c8cd4557877251ff8f66b27fd3413d
MarkJRust <markjrust@gmail.com>
2020-02-04 23:00:31 -0500

    mystery transaction

--- src/main/java/com/ordermeow/api/product/ProductRepository.java
+++ src/main/java/com/ordermeow/api/product/ProductRepository.java
@@ -2,8 +2,11 @@ package com.ordermeow.api.product;
 
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
 
-    void deleteByProductId(long productId);
+    @Transactional
+    void deleteByProductId(Long productId);
 
 }
