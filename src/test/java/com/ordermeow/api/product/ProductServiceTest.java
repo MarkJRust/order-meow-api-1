ea5817fc54ea8db174c2632605e3d1e84f576154
bennerv <10840174+bennerv@users.noreply.github.com>
2020-03-05 13:18:46 -1000

    Add additional tests for creating a new product

--- src/test/java/com/ordermeow/api/product/ProductServiceTest.java
+++ src/test/java/com/ordermeow/api/product/ProductServiceTest.java
@@ -7,7 +7,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Optional;
 
@@ -19,6 +22,9 @@ class ProductServiceTest {
     private static final String PRODUCT_NAME = "GARBAGE";
     private static final String PRODUCT_DESCRIPTION = "DESCRIPTION";
     private static final long PRODUCT_ID = 1;
+    private static final String FILE_NAME = "File Name.png";
+    private static final String IMAGE_TYPE = MediaType.IMAGE_JPEG_VALUE;
+    private static final byte[] IMAGE_BYTES = new byte[]{0x01, 0x02, 0x03};
 
     @InjectMocks
     private ProductService productService;
@@ -27,21 +33,35 @@ class ProductServiceTest {
     private ProductRepository productRepository;
 
     @Test
-    void createAlbum_happyPath() {
+    void createProduct_happyPath() throws IOException {
         ProductEntity expected = new ProductEntity();
-        expected.setProductName(PRODUCT_NAME);
+        MultipartFile file = mock(MultipartFile.class);
 
+        expected.setProductName(PRODUCT_NAME);
+        expected.setProductDescription(PRODUCT_DESCRIPTION);
+        expected.setProductPrice(BigDecimal.valueOf(1.00));
+        expected.setFileName(FILE_NAME);
+        expected.setFileType(IMAGE_TYPE);
+        expected.setProductImage(IMAGE_BYTES);
+
+        when(file.getOriginalFilename()).thenReturn(FILE_NAME);
+        when(file.getContentType()).thenReturn(IMAGE_TYPE);
+        when(file.getBytes()).thenReturn(IMAGE_BYTES);
         when(productRepository.save(expected)).thenReturn(expected);
 
-        //TODO - Null file
-        ProductEntity actual = productService.createProduct(expected, null);
+        ProductEntity actual = productService.createProduct(expected, file);
 
         Assertions.assertEquals(expected.getProductName(), actual.getProductName());
+        Assertions.assertEquals(expected.getFileName(), actual.getFileName());
+        Assertions.assertEquals(expected.getFileType(), actual.getFileType());
+        Assertions.assertEquals(expected.getProductDescription(), actual.getProductDescription());
+        Assertions.assertEquals(expected.getProductImage(), actual.getProductImage());
+        Assertions.assertEquals(expected.getProductPrice(), actual.getProductPrice());
         verify(productRepository, Mockito.times(1)).save(expected);
     }
 
     @Test
-    void createAlbum_nullNameThrowsException() {
+    void createProduct_nullNameThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName(null);
 
@@ -49,7 +69,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void createAlbum_emptyStringNameThrowsException() {
+    void createProduct_emptyStringNameThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName("");
 
@@ -57,7 +77,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void createAlbum_nullDescriptionThrowsException() {
+    void createProduct_nullDescriptionThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName(PRODUCT_NAME);
         badProduct.setProductDescription(null);
@@ -65,7 +85,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void createAlbum_emptyDescriptionThrowsException() {
+    void createProduct_emptyDescriptionThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName(PRODUCT_NAME);
         badProduct.setProductDescription("");
@@ -74,7 +94,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void createAlbum_nullPriceThrowsException() {
+    void createProduct_nullPriceThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName(PRODUCT_NAME);
         badProduct.setProductDescription(PRODUCT_DESCRIPTION);
@@ -83,7 +103,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void createAlbum_priceEqualsZeroThrowsException() {
+    void createProduct_priceEqualsZeroThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName(PRODUCT_NAME);
         badProduct.setProductDescription(PRODUCT_DESCRIPTION);
@@ -92,7 +112,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void createAlbum_priceLessThanZeroThrowsException() {
+    void createProduct_priceLessThanZeroThrowsException() {
         ProductEntity badProduct = new ProductEntity();
         badProduct.setProductName(PRODUCT_NAME);
         badProduct.setProductDescription(PRODUCT_DESCRIPTION);
@@ -101,7 +121,32 @@ class ProductServiceTest {
     }
 
     @Test
-    void getAlbum_happyPath() {
+    void createProduct_badFileNameThrowsException() {
+        MultipartFile file = mock(MultipartFile.class);
+        ProductEntity badProduct = new ProductEntity();
+        badProduct.setProductName(PRODUCT_NAME);
+        badProduct.setProductDescription(PRODUCT_DESCRIPTION);
+        badProduct.setProductPrice(BigDecimal.valueOf(1.00));
+        when(file.getOriginalFilename()).thenReturn("..");
+        Assertions.assertThrows(ProductExceptions.FileNameInvalid.class, () -> productService.createProduct(badProduct, file));
+    }
+
+    @Test
+    void createProduct_ioExceptionReadingFileBytes() throws IOException {
+        MultipartFile file = mock(MultipartFile.class);
+        ProductEntity badProduct = new ProductEntity();
+        badProduct.setProductName(PRODUCT_NAME);
+        badProduct.setProductDescription(PRODUCT_DESCRIPTION);
+        badProduct.setProductPrice(BigDecimal.valueOf(1.00));
+        when(file.getOriginalFilename()).thenReturn(FILE_NAME);
+        when(file.getContentType()).thenReturn(IMAGE_TYPE);
+        when(file.getBytes()).thenThrow(new IOException());
+
+        Assertions.assertThrows(ProductExceptions.InvalidFileException.class, () -> productService.createProduct(badProduct, file));
+    }
+
+    @Test
+    void getProduct_happyPath() {
         ProductEntity expected = new ProductEntity();
         expected.setProductId(PRODUCT_ID);
         expected.setProductName(PRODUCT_NAME);
@@ -113,7 +158,7 @@ class ProductServiceTest {
     }
 
     @Test
-    void getAlbum_notFound() {
+    void getProduct_notFound() {
         when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.empty());
         Assertions.assertThrows(ProductExceptions.ProductNotFound.class, () -> productService.getProduct(PRODUCT_ID));
     }
