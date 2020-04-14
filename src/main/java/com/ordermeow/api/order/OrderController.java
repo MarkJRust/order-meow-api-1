8380404e8ab49bdae4e2f8c581a113ef4adac06f
Cameron <cameron3pointer@gmail.com>
2020-04-14 18:18:16 -0400

    multiple

--- src/main/java/com/ordermeow/api/order/OrderController.java
+++ src/main/java/com/ordermeow/api/order/OrderController.java
@@ -106,7 +106,7 @@ public class OrderController {
         }
 
         expectedCost = expectedCost.setScale(2, RoundingMode.CEILING);
-
+        expectedCost = expectedCost.multiply(BigDecimal.valueOf(100));
 
         List<ProductEntity> orderedProducts = orderService.verifyTotal(productIds, expectedCost);
 
@@ -116,7 +116,7 @@ public class OrderController {
             lineItems.add(SessionCreateParams.LineItem.builder()
                     .setName(product.getProductName())
                     .setDescription(product.getProductDescription())
-                    .setAmount(product.getProductPrice().longValue()*100)
+                    .setAmount(product.getProductPrice().longValue())
                     .setCurrency("usd")
                     .setQuantity(1L)
                     .build());
